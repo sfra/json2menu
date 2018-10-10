8fa098407658bdad21a7b06c159604d214d955dd
Szymon Frankowski <sz.frankyy@gmail.com>
2018-10-10 15:29:46 +0200

    Links fixed

--- jquery.menu.js
+++ jquery.menu.js
@@ -1,103 +1,76 @@
+(function($) {
 
-console.log(typeof {});
-   
-  
+  $.fn.menu = function(options) {
 
-(function($){
 
-$.fn.menu=function(options){
-  
-  
-   var fillUL=function(jsonCode,dest){
-   //  debugger; 
-     
-//     dest.html("AAAA");
-      var i=0;
-     if (jsonCode instanceof Array) {
-         for (i = 0;i<jsonCode.length; i+=1  ) {
-            var $LI=$("<li></li>");
-            $LI.find("*").css({"display":"none"});
-            
-               if (jsonCode[i].children!=undefined) {
-                  $LI.addClass("hasChildren");
-               } else {
-                  $LI.addClass("noChildren");
-                  
-                  
-               }
-               
-               console.log(jsonCode[i].click);
-               if (jsonCode[i].click!=undefined) {
-                  console.log(jsonCode[i].click);
-               }
-               
-            
-            $LI.text(jsonCode[i].text);
-            fillLI(jsonCode[i],$LI);
-            dest.append($LI);
-         }
-      
-     }
-     
-      
+    var fillUL = function(jsonCode, dest) {
 
-    };
-    
-    
-    var fillLI=function(jsonCode,dest){
-      if (typeof jsonCode==="object") {
-         if (jsonCode.children!=undefined) {
-            
-            
-            dest.on("click",function(e){
-               $(this).children("*").fadeIn().children().fadeIn();
-               $(this).siblings("li").find("*").fadeOut();
-            });
-            var $UL=$("<ul></ul>").css({"display":"none"});
-            $UL.find("*").css({"display":"none"});
-            fillUL(jsonCode.children,$UL);
-            dest.append($UL);
-         }
+      var i = 0;
+      if (jsonCode instanceof Array) {
+        for (i = 0; i < jsonCode.length; i += 1) {
+          var $LI = $("<li></li>");
+          $LI.find("*").css({
+            "display": "none"
+          });
+
+          if (jsonCode[i].children != undefined) {
+            $LI.addClass("hasChildren");
+          } else {
+            $LI.addClass("noChildren");
+          }
+
+          $LI.text(jsonCode[i].text);
+          fillLI(jsonCode[i], $LI);
+          dest.append($LI);
+        }
       }
-      
     };
-    
-    
-  
-  
-
-                    
-  var opt=$.extend({},$.fn.menu.defaults,options);
-  
-//    debugger;
-  
-  
-  
-  
-  $.getJSON("http://localhost/json2menu/menu.json",function(data){
-
-//    console.log(data[0].click);  
-        fillUL(data,$("ul"));
-    
-    }); 
-  return this; 
-
-}
-
-$.fn.menu.defaults={
-   enlarge: $.fn.fadeTo,
-   openSpeed: 500,
-   openbg: "#777777",
-   mouseoutOpacity: 0.7,
-   mouseOut: {"opacity":0.7},
-   openProp: {"speed":500,"css":{"opacity":0.7},"background-color":"#777777"}
-   
-}
 
 
-}( jQuery ));
+    var fillLI = function(jsonCode, dest) {
+      if (typeof jsonCode === "object") {
+        if (jsonCode.children != undefined) {
+
+          dest.on("click", function(e) {
+            $(this).children("*").fadeIn().children().fadeIn();
+            $(this).siblings("li").find("*").fadeOut();
+          });
+          var $UL = $("<ul></ul>").css({
+            "display": "none"
+          });
+          $UL.find("*").css({
+            "display": "none"
+          });
+          fillUL(jsonCode.children, $UL);
+          dest.append($UL);
+        }
+      }
+    };
 
 
+    var opt = $.extend({}, $.fn.menu.defaults, options);
 
+    $.getJSON("menu.json", function(data) {
+      fillUL(data, $("ul"));
+    });
+    return this;
+  }
 
+  $.fn.menu.defaults = {
+    enlarge: $.fn.fadeTo,
+    openSpeed: 500,
+    openbg: "#777777",
+    mouseoutOpacity: 0.7,
+    mouseOut: {
+      "opacity": 0.7
+    },
+    openProp: {
+      "speed": 500,
+      "css": {
+        "opacity": 0.7
+      },
+      "background-color": "#777777"
+    }
+  }
 
+}(jQuery));
